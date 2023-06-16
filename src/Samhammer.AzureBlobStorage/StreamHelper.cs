66c7df0adb393051c1c667aab7ed66c8fca23eb5
Khoa <khoa.nguyen@samhammer.de>
2023-06-16 15:08:18 +0700

    ASMO-5226 Fix null check

--- src/Samhammer.AzureBlobStorage/StreamHelper.cs
+++ src/Samhammer.AzureBlobStorage/StreamHelper.cs
@@ -5,10 +5,18 @@ namespace Samhammer.AzureBlobStorage
 {
     public class StreamHelper
     {
+        private const int BlockSize = 1024;
+        private const int LargeBufferMultiple = 1024 * 1024;
+        private const int MaxBufferSize = 16 * LargeBufferMultiple;
+        private const long DefaultMaxSmallPoolFreeBytes = MaxBufferSize * 4;
+        private const long DefaultMaxLargePoolFreeBytes = 100 * BlockSize;
         private static string maxSmallPoolFreeBytes = Environment.GetEnvironmentVariable("MAX_SMALLPOOL_FREEBYTES");
         private static string maxLargePoolFreeBytes = Environment.GetEnvironmentVariable("MAX_LARGEPOOL_FREEBYTES");
         public static readonly RecyclableMemoryStreamManager StreamManager = new RecyclableMemoryStreamManager(
-            string.IsNullOrEmpty(maxSmallPoolFreeBytes) ? long.Parse(maxSmallPoolFreeBytes) : 1000000,
-            string.IsNullOrEmpty(maxLargePoolFreeBytes) ? long.Parse(maxLargePoolFreeBytes) : 10000000);
+            BlockSize,
+            LargeBufferMultiple,
+            MaxBufferSize,
+            string.IsNullOrEmpty(maxSmallPoolFreeBytes) ? DefaultMaxSmallPoolFreeBytes : long.Parse(maxSmallPoolFreeBytes),
+            string.IsNullOrEmpty(maxLargePoolFreeBytes) ? DefaultMaxLargePoolFreeBytes : long.Parse(maxLargePoolFreeBytes));
     }
 }
