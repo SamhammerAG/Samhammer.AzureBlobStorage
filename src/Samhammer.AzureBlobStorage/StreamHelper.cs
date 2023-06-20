e0378fe913295ff5d10dd20f03c08217b4a0bec9
Khoa <khoa.nguyen@samhammer.de>
2023-06-20 10:26:00 +0700

    ASMO-5226 Memory stream manager

--- src/Samhammer.AzureBlobStorage/StreamHelper.cs
+++ src/Samhammer.AzureBlobStorage/StreamHelper.cs
@@ -5,17 +5,11 @@ namespace Samhammer.AzureBlobStorage
 {
     public class StreamHelper
     {
-        private const int BlockSize = 1024;
-        private const int LargeBufferMultiple = 1024 * 1024;
-        private const int MaxBufferSize = 16 * LargeBufferMultiple;
-        private const long DefaultMaxSmallPoolFreeBytes = MaxBufferSize * 4;
-        private const long DefaultMaxLargePoolFreeBytes = 100 * BlockSize;
+        private const long DefaultMaxSmallPoolFreeBytes = 1000000;
+        private const long DefaultMaxLargePoolFreeBytes = 10000000;
         private static string maxSmallPoolFreeBytes = Environment.GetEnvironmentVariable("MAX_SMALLPOOL_FREEBYTES");
         private static string maxLargePoolFreeBytes = Environment.GetEnvironmentVariable("MAX_LARGEPOOL_FREEBYTES");
         public static readonly RecyclableMemoryStreamManager StreamManager = new RecyclableMemoryStreamManager(
-            BlockSize,
-            LargeBufferMultiple,
-            MaxBufferSize,
             string.IsNullOrEmpty(maxSmallPoolFreeBytes) ? DefaultMaxSmallPoolFreeBytes : long.Parse(maxSmallPoolFreeBytes),
             string.IsNullOrEmpty(maxLargePoolFreeBytes) ? DefaultMaxLargePoolFreeBytes : long.Parse(maxLargePoolFreeBytes));
     }
